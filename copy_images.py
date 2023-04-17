fd7dad9cbf15233cb569eba59e92a8d4d964dd7f
Paul Emerson <emersunn@gmail.com>
2023-04-17 11:39:03 -0700

    Create copy_images.py

---
+++ copy_images.py
@@ -0,0 +1,31 @@
+import os
+import shutil
+from pathlib import Path
+
+# Set the source and destination directories
+source_dir = "/Users/username/Pictures/Photos Library.photoslibrary"
+dest_dir = "/Volumes/driveName/ApplePhotosExport"
+
+# Get all files in the source directory
+all_files = [f for f in Path(source_dir).rglob("*") if f.is_file()]
+
+# Filter only image files
+image_extensions = {'.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff'}
+image_files = [f for f in all_files if f.suffix.lower() in image_extensions]
+
+# Create the destination directory if it doesn't exist
+os.makedirs(dest_dir, exist_ok=True)
+
+# Copy and rename image files to the destination directory
+counter = 1
+for img in image_files:
+    # Construct the new filename
+    new_name = f"image{counter}{img.suffix}"
+
+    # Copy and rename the image
+    shutil.copy2(img, os.path.join(dest_dir, new_name))
+
+    # Increment the counter
+    counter += 1
+
+print("Images copied and renamed successfully.")
